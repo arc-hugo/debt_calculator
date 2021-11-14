0ceb2f4e0e86480d11a629cfff27d29fc8fbff2c
Hugo Barral <hugo.barral@outlook.fr>
2021-11-15 00:27:59 +0100

    Creating the Ford-Fulkerson module

---
+++ src/ford.mli
@@ -0,0 +1,7 @@
+(* Module of Ford-Fulkerson algorithm.
+ * To be used with the Graph module. *)
+open Graph
+
+(* Type of flow running through the graph's arcs.
+ * Meant to be used as a label. *)
+type 'a flow
