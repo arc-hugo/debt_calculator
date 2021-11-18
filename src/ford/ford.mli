740d2488c0f45f24a3ae9196fe51ba026b318906
Hugo Barral <hugo.barral@outlook.fr>
2021-11-18 15:44:28 +0100

    debug on FordFulkerson module

--- src/ford/ford.mli
+++ src/ford/ford.mli
@@ -1,6 +1,6 @@
 (* Module of Ford-Fulkerson algorithm.
  * To be used with the Graph module. *)
-open Graph
+(* open Graph *)
 
 (* Type of flow running through the graph's arcs.
  * Meant to be used as a label. *)
