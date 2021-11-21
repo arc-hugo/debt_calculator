46d2f49e00c0b125a508d3a8b259436df6e8564f
Hugo Barral <hugo.barral@outlook.fr>
2021-11-21 23:09:55 +0100

    adding new_flow_graph function

--- src/mod/ford/ford.ml
+++ src/mod/ford/ford.ml
@@ -8,27 +8,62 @@ type flow = {
    max: int
 }
 
+
+let new_flow_graph (gr: int graph) = gmap gr (fun v -> {current = 0; max = v})
+
+let rec constr_path gr = function
+   | [] | _::[] -> []
+   | s::t::rest -> match (find_arc gr s t) with
+      | Some v -> (s,t,v)::(constr_path gr (t::rest))
+      | None -> raise (FordFulkerson_exception "error find arc")
+
+let rec dfs (gr: flow graph) (s: id) (t: id) (visit: id list) =
+   let rec succ (gr: flow graph) (s: id) (t: id) (visit: id list) = function
+      | [] -> []
+      | h::rest -> let path = dfs gr h t visit in
+      if (List.mem t path) then 
+         s::path
+      else
+         succ gr s t visit rest
+   in
+   if (s = t) then 
+      [t]
+   else if not(List.mem s visit) && not(List.mem t visit) then
+      let succ_nodes = e_fold gr (fun l _ n2 f -> if (f.current - f.max) > 0 then n2::l else l) [] in
+      succ gr s t (s::visit) succ_nodes
+   else
+      []
+
 let find_path (gr: flow graph) (s: id) (t: id) =
-   let rec dfs (gr: flow graph) (s: id) (t: id) (visit: id list) =
-   	let rec succ (gr: flow graph) (s: id) (t: id) (visit: id list) = function
-			| [] -> []
-			| h::rest -> let path = dfs gr h t visit in
-				if (List.mem t path)
-				then s::path
-				else succ gr s t visit rest
-   	in
-      if (s = t) then 
-			[t]
-      else if not(List.mem s visit) && not(List.mem t visit) then
-         let succ_nodes = e_fold gr (fun l _ n2 f -> if (f.current - f.max) > 0 then n2::l else l) [] in
-         succ gr s t (s::visit) succ_nodes
-		else
-			[]
-   in
-   let rec constr_path gr = function
-      | [] | _::[] -> []
-      | s::t::rest -> (find_arc gr s t)::(constr_path gr (t::rest))
-   in
    if (not(node_exists gr s)) || (not(node_exists gr t))
    then raise (FordFulkerson_exception "start or target not found")
-	else constr_path gr (dfs gr s t [])
+   else constr_path gr (dfs gr s t [])
+
+let rec min = function
+   | [] -> 0
+   | (_,_,f)::t -> let v1 = min t and v2 = (f.max - f.current) in
+   if v1 < v2 then
+      v1
+   else
+      v2
+
+let ford_fulkerson (gr: flow graph) (s: id) (t: id) =
+   let rec aux (gr: flow graph) (s: id) (t: id) =
+      let path = find_path gr s t in
+      if not(path = []) then
+         let m = min path in
+         let ngr = e_fold gr 
+            (fun ngr i1 i2 f -> 
+               if List.mem (i1,i2,f) path
+               then new_arc ngr i1 i2 {f with current=f.current+m}
+               else new_arc ngr i1 i2 f
+               )
+            (clone_nodes gr)
+         in
+         aux ngr s t
+      else
+         gr
+   in
+   (*Init flow*)
+   let igr = gmap gr (fun f -> { f with current = 0 }) in
+   aux igr s t
--- src/mod/ford/ford.mli
+++ src/mod/ford/ford.mli
@@ -1,7 +1,17 @@
 (* Module of Ford-Fulkerson algorithm.
  * To be used with the Graph module. *)
-(* open Graph *)
+open Graph
 
 (* Type of flow running through the graph's arcs.
  * Meant to be used as a label. *)
 type flow
+
+
+(**************  CONSTRUCTORS  **************)
+(* Create a flow graph from a int graph. *)
+val new_flow_graph : int graph -> flow graph
+
+(**************  ALGORITHM  **************)
+(* Apply Ford-Fulkerson algorithm to a glow graph between a start and a target.
+ * @raise FordFulkerson_exception if start or target not found. *)
+val ford_fulkerson : flow graph -> id -> id -> flow graph
