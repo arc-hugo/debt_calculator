0298633eb2a5325d26a51176070a9115eee44aaa
Hugo Barral <hugo.barral@outlook.fr>
2021-11-22 15:27:01 +0100

    add getters

--- src/mod/ford/ford.mli
+++ src/mod/ford/ford.mli
@@ -6,12 +6,24 @@ open Graph
  * Meant to be used as a label. *)
 type flow
 
+exception Flow_exception of string
+exception FordFulkerson_exception of string
 
-(**************  CONSTRUCTORS  **************)
+(**************  CONSTRUCTOR  **************)
 (* Create a flow graph from a int graph. *)
 val new_flow_graph : int graph -> flow graph
 
+(**************  GETTERS  **************)
+(* Return current flow of an arc between id1 and id2.
+ * Return None if the arc does not exist.
+ * @raise Flow_exception id id1 is unknown. *)
+val current_flow : flow graph -> id -> id -> int option
+(* Return max flow of an arc between id1 and id2.
+ * Return None if the arc does not exist.
+ * @raise Flow_exception id id1 is unknown. *)
+val max_flow : flow graph -> id -> id -> int option
+
 (**************  ALGORITHM  **************)
-(* Apply Ford-Fulkerson algorithm to a glow graph between a start and a target.
+(* Apply Ford-Fulkerson algorithm to a glow graph between a start and a target nodes.
  * @raise FordFulkerson_exception if start or target not found. *)
 val ford_fulkerson : flow graph -> id -> id -> flow graph
