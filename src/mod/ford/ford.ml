3ad58a4969f4fe9e1a7b466e56e44e8a0b6804d2
Hugo Barral <hugo.barral@outlook.fr>
2021-11-22 15:27:29 +0100

    getters + correcting dfs

--- src/mod/ford/ford.ml
+++ src/mod/ford/ford.ml
@@ -1,6 +1,7 @@
 open Graph
 open Tools
 
+exception Flow_exception of string
 exception FordFulkerson_exception of string
 
 type flow = {
@@ -8,44 +9,53 @@ type flow = {
    max: int
 }
 
+let get_flow (gr: flow graph) (id1: id) (id2: id) =
+   try
+      match (find_arc gr id1 id2) with
+         | Some f -> Some f
+         | None -> None
+   with
+      Graph_error s -> raise (Flow_exception s)
 
-let new_flow_graph (gr: int graph) = gmap gr (fun v -> {current = 0; max = v})
+let current_flow (gr: flow graph) (id1: id) (id2: id) =
+   match (get_flow gr id1 id2) with
+      | Some f -> Some f.current
+      | None -> None
 
-let rec constr_path gr = function
+let max_flow (gr: flow graph) (id1: id) (id2: id) =
+   match (get_flow gr id1 id2) with
+      | Some f -> Some f.max
+      | None -> None
+
+let new_flow_graph (gr: int graph) = gmap gr (fun v -> {current=0; max=v})
+
+let rec constr_path (gr: flow graph) = function
    | [] | _::[] -> []
    | s::t::rest -> match (find_arc gr s t) with
-      | Some v -> (s,t,v)::(constr_path gr (t::rest))
+      | Some f -> (s,t,f)::(constr_path gr (t::rest))
       | None -> raise (FordFulkerson_exception "error find arc")
 
 let rec dfs (gr: flow graph) (s: id) (t: id) (visit: id list) =
-   let rec succ (gr: flow graph) (s: id) (t: id) (visit: id list) = function
-      | [] -> []
-      | h::rest -> let path = dfs gr h t visit in
-      if (List.mem t path) then 
-         s::path
-      else
-         succ gr s t visit rest
-   in
-   if (s = t) then 
-      [t]
-   else if not(List.mem s visit) && not(List.mem t visit) then
-      let succ_nodes = e_fold gr (fun l _ n2 f -> if (f.current - f.max) > 0 then n2::l else l) [] in
-      succ gr s t (s::visit) succ_nodes
+   if (List.mem s visit) || (List.mem t visit)
+   then visit
    else
-      []
+      let succ = e_fold gr 
+         (fun l n1 n2 f -> if (n1 = s) && not(List.mem n2 visit) && (f.max - f.current) > 0 then n2::l else l)
+         []
+      in
+      List.fold_left (fun visit suiv -> dfs gr suiv t visit) (s::visit) succ
 
 let find_path (gr: flow graph) (s: id) (t: id) =
    if (not(node_exists gr s)) || (not(node_exists gr t))
    then raise (FordFulkerson_exception "start or target not found")
-   else constr_path gr (dfs gr s t [])
+   else 
+      let path = List.rev(dfs gr s t []) in
+      constr_path gr path
 
 let rec min = function
-   | [] -> 0
+   | [] -> Int.max_int
    | (_,_,f)::t -> let v1 = min t and v2 = (f.max - f.current) in
-   if v1 < v2 then
-      v1
-   else
-      v2
+      if v1 < v2 then v1 else v2
 
 let ford_fulkerson (gr: flow graph) (s: id) (t: id) =
    let rec aux (gr: flow graph) (s: id) (t: id) =
@@ -53,11 +63,11 @@ let ford_fulkerson (gr: flow graph) (s: id) (t: id) =
       if not(path = []) then
          let m = min path in
          let ngr = e_fold gr 
-            (fun ngr i1 i2 f -> 
+            (fun ngr i1 i2 f ->
                if List.mem (i1,i2,f) path
                then new_arc ngr i1 i2 {f with current=f.current+m}
                else new_arc ngr i1 i2 f
-               )
+            )
             (clone_nodes gr)
          in
          aux ngr s t
