8817f27b69313907963599032e2241258e1bc33c
Hugo Barral <hugo.barral@outlook.fr>
2021-12-03 09:31:02 +0100

    adding debt calculation module

---
+++ src/mod/debt/debt.mli
@@ -0,0 +1,16 @@
+(* Read a payment record between a set of person.
+ * Calculate the money owed between each other. *)
+
+(* The fomat of file used is :
+   Name1 MoneyPaid1\n
+   Name2 MoneyPaid2\n
+   ....
+   NameN MoneyPaidN
+   *)
+
+open Graph
+open Ford
+
+(**************  ALGORITHM  **************)
+(* Read the payment record and return a flow graph representing debt of each person. *)
+val debt string -> flow graph
