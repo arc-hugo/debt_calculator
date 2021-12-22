ee46b05c27882f467473c79b4f5b8cf901b80ad8
Hugo Barral <hugo.barral@outlook.fr>
2021-12-22 22:07:58 +0100

    change error code

--- bin/debt.ml
+++ bin/debt.ml
@@ -2,13 +2,13 @@ open Debt
 
 let () =
    if Array.length Sys.argv <> 3 then
-    begin
-      Printf.printf
+      begin
+         Printf.printf
         "\n ✻  Usage: %s infile outfile\n\n%s%!" Sys.argv.(0)
         ("    🟄  infile  : input file containing a payment record\n" ^
          "    🟄  outfile : output file in which the result should be written.\n\n") ;
-      exit 0
-    end ;
+      exit 1
+      end ;
 
     let infile = Sys.argv.(1) 
     and outfile = Sys.argv.(2)
